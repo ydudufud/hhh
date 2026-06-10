f0cade1150c93464925dbd1398b5f70b0c2e124a
bashar <wwwbasharw2@gmail.com>
2026-06-11 00:08:31 +0300

    Update MainActivity to use modern Flutter embedding

--- android/app/src/main/kotlin/com/example/target_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/target_app/MainActivity.kt
@@ -1,5 +1,19 @@
 package com.example.target_app
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "com.example.target/service")
+            .setMethodCallHandler { call, result ->
+                when (call.method) {
+                    "start" -> result.success(true)
+                    else -> result.notImplemented()
+                }
+            }
+    }
+}
